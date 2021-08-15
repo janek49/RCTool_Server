1af6b960e766c58cfa1503f0715047d41b9fd284
Jan Bartula <jan.bartula@outlook.com>
2021-08-16 00:38:20 +0200

    wpf seperation

---
+++ ServerUI/UIComponent/IconTreeViewItem.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ServerUI.UIComponent
+{
+    public class IconTreeViewItem : TreeViewItem
+    {
+        ImageSource iconSource;
+        TextBlock textBlock;
+        Image icon;
+
+        protected override void OnMouseEnter(MouseEventArgs e)
+        {
+            base.OnMouseEnter(e);
+        }
+
+        public IconTreeViewItem()
+        {
+            StackPanel stack = new StackPanel();
+            stack.Orientation = Orientation.Horizontal;
+            Header = stack;
+            //Uncomment this code If you want to add an Image after the Node-HeaderText
+            //textBlock = new TextBlock();
+            //textBlock.VerticalAlignment = VerticalAlignment.Center;
+            //stack.Children.Add(textBlock);
+            icon = new Image();
+            icon.VerticalAlignment = VerticalAlignment.Center;
+            icon.Height = 16;
+            icon.Width = 16;
+            icon.Margin = new Thickness(0, 0, 4, 0);
+            icon.Source = iconSource;
+            stack.Children.Add(icon);
+            //Add the HeaderText After Adding the icon
+            textBlock = new TextBlock();
+            textBlock.VerticalAlignment = VerticalAlignment.Center;
+            stack.Children.Add(textBlock);
+            
+        }
+
+        public ImageSource Icon
+        {
+            set
+            {
+                iconSource = value;
+                icon.Source = iconSource;
+            }
+            get
+            {
+                return iconSource;
+            }
+        }
+
+        protected override void OnUnselected(RoutedEventArgs args)
+        {
+            base.OnUnselected(args);
+            icon.Source = iconSource;
+        }
+
+        protected override void OnSelected(RoutedEventArgs args)
+        {
+            base.OnSelected(args);
+            icon.Source = iconSource;
+        }
+
+        /// <summary>
+        /// Gets/Sets the HeaderText of TreeViewWithIcons
+        /// </summary>
+        public string HeaderText
+        {
+            set
+            {
+                textBlock.Text = value;
+            }
+            get
+            {
+                return textBlock.Text;
+            }
+        }
+
+        protected override void OnInitialized(EventArgs e)
+        {
+            base.OnInitialized(e);
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        { 
+            base.OnRender(drawingContext);
+        }
+
+        protected override void OnRenderSizeChanged(SizeChangedInfo sizeInfo)
+        {
+           
+            base.OnRenderSizeChanged(sizeInfo);
+        }
+
+        protected override void OnChildDesiredSizeChanged(UIElement child)
+        {
+           
+            base.OnChildDesiredSizeChanged(child);
+        }
+    }
+}
---
+++ ServerUI/WindowClientManager.xaml.cs
@@ -0,0 +1,114 @@
+﻿/************************************************************************
+   AvalonDock
+
+   Copyright (C) 2007-2013 Xceed Software Inc.
+
+   This program is provided to you under the terms of the Microsoft Public
+   License (Ms-PL) as published at https://opensource.org/licenses/MS-PL
+ ************************************************************************/
+
+using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using AvalonDock.Layout;
+using System.Diagnostics;
+using System.IO;
+using AvalonDock.Layout.Serialization;
+using AvalonDock;
+using System.Diagnostics.CodeAnalysis;
+
+namespace ServerUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class WindowClientManager : Window
+    {
+
+        public WindowClientManager()
+        {
+            InitializeComponent();
+            DataContext = this; 
+        }
+ 
+
+        #region FocusedElement
+
+        /// <summary>
+        /// FocusedElement Dependency Property
+        /// </summary>
+        public static readonly DependencyProperty FocusedElementProperty =
+            DependencyProperty.Register("FocusedElement", typeof(string), typeof(WindowClientManager),
+                new FrameworkPropertyMetadata((IInputElement)null));
+
+        /// <summary>
+        /// Gets or sets the FocusedElement property.  This dependency property 
+        /// indicates ....
+        /// </summary>
+        public string FocusedElement
+        {
+            get => (string)GetValue(FocusedElementProperty);
+            set => SetValue(FocusedElementProperty, value);
+        }
+
+        #endregion
+
+        private void OnLayoutRootPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            var activeContent = ((LayoutRoot)sender).ActiveContent;
+            if (e.PropertyName == "ActiveContent")
+            {
+                Debug.WriteLine(string.Format("ActiveContent-> {0}", activeContent));
+            }
+        }
+ 
+
+        private void AddTwoDocuments_click(object sender, RoutedEventArgs e)
+        {
+            var firstDocumentPane = dockManager.Layout.Descendents().OfType<LayoutDocumentPane>().FirstOrDefault();
+
+            if (firstDocumentPane != null)
+            {
+                LayoutDocument doc = new LayoutDocument
+                {
+                    Title = "Test1"
+                };
+                firstDocumentPane.Children.Add(doc);
+
+                LayoutDocument doc2 = new LayoutDocument
+                {
+                    Title = "Test2"
+                };
+                firstDocumentPane.Children.Add(doc2);
+            }
+
+            var leftAnchorGroup = dockManager.Layout.LeftSide.Children.FirstOrDefault();
+            if (leftAnchorGroup == null)
+            {
+                leftAnchorGroup = new LayoutAnchorGroup();
+                dockManager.Layout.LeftSide.Children.Add(leftAnchorGroup);
+            }
+
+            leftAnchorGroup.Children.Add(new LayoutAnchorable() { Title = "New Anchorable" });
+
+        }
+
+        private void DockManager_DocumentClosing(object sender, DocumentClosingEventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to close the document?", "AvalonDock Sample", MessageBoxButton.YesNo) == MessageBoxResult.No)
+                e.Cancel = true;
+        }
+
+        private void EventHandler_TreeView(object sender, MouseButtonEventArgs e)
+        {
+           if(sender == tviWebcam)
+            {
+                MessageBox.Show("Webcam");
+            }
+        }
+    }
+}
